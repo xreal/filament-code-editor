2de424faec703659accce2de05b74bbdd5a86fa8
Wiebe Nieuwenhuis <wiebenieuwenhuis@gmail.com>
2023-10-17 19:35:35 +0200

    Update FilamentCodeEditorServiceProvider.php

--- src/FilamentCodeEditorServiceProvider.php
+++ src/FilamentCodeEditorServiceProvider.php
@@ -5,21 +5,19 @@ namespace SebastiaanKloos\FilamentCodeEditor;
 use Filament\Facades\Filament;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
 
 class FilamentCodeEditorServiceProvider extends PackageServiceProvider
 {
-    protected array $beforeCoreScripts = [
-        'filament-tools' => __DIR__.'/../dist/filament-tools.js',
-    ];
-
     public function configurePackage(Package $package): void
     {
         $package->name('filament-code-editor')->hasViews();
     }
 
     public function bootingPackage() {
-        Filament::serving(function () {
-            Filament::registerScripts($this->beforeCoreScripts, true);
-        });
+        FilamentAsset::register([
+            Js::make('filament-tools', __DIR__.'/../dist/filament-tools.js'),
+        ]);
     }
 }
